bd861dc7f3f2ad0de58a7f5846a60ae5098b6d3c
wandersondv <87458855+wandersondv@users.noreply.github.com>
2023-04-19 21:22:51 +0000

    correction app database

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
+
 
 app = Flask(__name__)
 
+"""
 JOBS = [
   {
     'id': 1,
@@ -27,16 +30,20 @@ JOBS = [
     'salary': '$150,000'
   }
 ]
+"""
 
+      
 @app.route("/")
 def hello_jovian():
+    jobs_list = load_jobs_from_db()
     return render_template('home.html', 
-                           jobs=JOBS, 
+                           jobs=jobs_list, 
                            company_name='Jovian')
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,18 +1,32 @@
 from sqlalchemy import create_engine, text
+#import os
 
-db_connection_string = "mysql+pymysql://atyoq63j1r7wyjb8u3h9:pscale_pw_PvuYsU6LD9fQ9zQWecAoFBkxoWZgYw5C4MQ4H0FbLrZ@aws.connect.psdb.cloud/wandersoncareers?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://jtrybbz7sun03a3albi0:pscale_pw_AJ6fHDZ3AfDT2TOcyb5nEcE08LJ1DSwApw7ewSu7gNz@aws.connect.psdb.cloud/wandersoncareers?charset=utf8mb4"
 
-engine = create_engine( db_connection_string, 
+#db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(
+  db_connection_string, 
   connect_args={
     "ssl": {
       "ssl_ca": "/etc/ssl/cert.pem"
     }
   })
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
-  print("type(result):", type(result))
-  result_all = result.all()
-  print("type(result.all())",type(result_all))
-  print("result.all():", result_all)
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+          result = conn.execute(text("select * from jobs"))
+          results_as_dict = result.mappings().all()
+          jobs = []
+          for row in results_as_dict:
+            jobs.append(row)
+          
+          return jobs
+    
+  
+
+    
+      
+    
   
